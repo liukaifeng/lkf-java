c601d9dd14c99bb295029adf3421c6209d779e24
kaifeng <kaifeng_5liu@163.com>
2019-02-20 17:51:36 +0800

    提交代码

--- algorithm/src/main/java/com/lkf/sort/InsertSort.java
+++ algorithm/src/main/java/com/lkf/sort/InsertSort.java
@@ -9,6 +9,30 @@ public class InsertSort {
     public static void main(String[] args) {
 
         int[] arrays = {1, 8, 9, 3, 5, 6};
+
+        System.out.println("选择排序结果：" + Arrays.toString(sort(arrays)));
+    }
+
+    public static int[] sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            //插入的数
+            int insertVal = arr[i];
+            //被插入的位置(准备和前一个数比较)
+            int index = i - 1;
+            //如果插入的数比被插入的数小
+            while (index >= 0 && insertVal < arr[index]) {
+                //将把 arr[index] 向后移动
+                arr[index + 1] = arr[index];
+                //让 index 向前移动
+                index--;
+            }
+            //把插入的数放入合适位置
+            arr[index + 1] = insertVal;
+        }
+        return arr;
+    }
+
+    public int[] sort2(int[] arrays) {
         //临时变量
         int temp;
 
@@ -32,6 +56,6 @@ public class InsertSort {
             arrays[j + 1] = temp;
 
         }
-        System.out.println("选择排序结果：" + Arrays.toString(arrays));
+        return arrays;
     }
 }
---
+++ algorithm/src/main/java/com/lkf/sort/MySortDemo.java
@@ -0,0 +1,13 @@
+package com.lkf.sort;
+
+/**
+ * to do desc
+ *
+ * @author kaifeng
+ * @date 2019/2/20
+ */
+public class MySortDemo {
+    public static void main(String[] args) {
+
+    }
+}
