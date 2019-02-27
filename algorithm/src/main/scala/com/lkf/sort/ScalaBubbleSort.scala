8bdced798d85f6bac4d6be7996e887c8dfe79b63
刘凯峰 <liukaifeng@tcsl.com.cn>
2019-02-27 21:01:06 +0800

    冒泡排序，scala实现

--- algorithm/src/main/scala/com/lkf/sort/ScalaBubbleSort.scala
+++ algorithm/src/main/scala/com/lkf/sort/ScalaBubbleSort.scala
@@ -3,18 +3,20 @@ package com.lkf.sort
 
 object ScalaBubbleSort {
   def main(args: Array[String]): Unit = {
-
-    println(sort(Array(1, 9, 3, 4, 0, 7, 5, 6)).mkString)
+    println(sort(Array(1, 9, 3, 4, 0, 7, 5, 6)))
   }
 
   def sort(arrs: Array[Int]): Array[Int] = {
     for (i <- arrs.indices) {
+      var noExchange=true
       for (j <- 0 until arrs.length - i - 1) {
         if (arrs(j) > arrs(j + 1)) {
           val temp = arrs(j)
           arrs.update(j, arrs(j + 1))
           arrs.update(j + 1, temp)
+          noExchange=false
         }
+        
       }
     }
     arrs
